d9ccaf3d4731ed957747d39ae9abce0daa2154bf
AHX <vladis9484@gmail.com>
2023-05-26 18:25:45 +0300

    Добавьте файлы проекта.

---
+++ DDTasks/Program.cs
@@ -0,0 +1,73 @@
+﻿using DDTasks_Infrastructure;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace DDTasks
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NonParallelTask();
+            ParallelTask();
+        }
+        //Task 1
+        public static void NonParallelTask()
+        {
+            var inf = new Infrastructure();
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                string text = reader.ReadToEnd();
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                MethodInfo meth = inf.GetType().
+                    GetMethod("FileAnalyzer", BindingFlags.NonPublic | BindingFlags.Instance);
+                var result = meth.Invoke(inf, new object[] { text });
+                stopwatch.Stop();
+                TimeSpan time = stopwatch.Elapsed;
+                string elapsedTimeNonParallel = String.Format("{0:0}", time.Seconds);
+                Console.WriteLine($"Private method: {elapsedTimeNonParallel} sec");
+
+
+                using (StreamWriter writer = new StreamWriter("NonParallelOutput.txt"))
+                {
+                    foreach (var word in (Dictionary<string, int>)result)
+                    {
+                        writer.WriteLine($"{word.Key}\t{word.Value}");
+
+                    }
+                }
+
+            }
+        }
+        //Task 2
+        public static void ParallelTask()
+        {
+            var inf = new Infrastructure();
+            using (StreamReader reader = new StreamReader("text.txt"))
+            {
+                string text = reader.ReadToEnd();
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                MethodInfo meth = inf.GetType().
+                    GetMethod("ParallelFileAnalyzer", BindingFlags.Public | BindingFlags.Instance);
+                var result = meth.Invoke(inf, new object[] { text });
+                stopwatch.Stop();
+                TimeSpan time = stopwatch.Elapsed;
+                string elapsedTimeNonParallel = String.Format("{0:00}", time.Milliseconds);
+                Console.WriteLine($"Public method: {elapsedTimeNonParallel}ms");
+
+
+                using (StreamWriter writer = new StreamWriter("ParallelOutput.txt"))
+                {
+                    foreach (var word in (Dictionary<string, int>)result)
+                    {
+                        writer.WriteLine($"{word.Key}\t{word.Value}");
+
+                    }
+                }
+
+            }
+        }
+    }
+}
---
+++ DDTasks_Infrastructure/Infrastructure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace DDTasks_Infrastructure
+{
+    public class Infrastructure
+    {
+        private Dictionary<string, int> FileAnalyzer(string text)
+        {
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r', '\t', '[', ']', '(', ')', '"' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries).Distinct().ToArray();
+            
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                int count = words.Where(ex => ex.Equals(word)).Count();
+                dict.Add(word, count);
+            }
+            return dict;
+        }
+        public Dictionary<string, int> ParallelFileAnalyzer(string text)
+        {
+            object locker = new object();
+            char[] separators = new char[] { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r', '\t', '[', ']', '(', ')', '"' };
+            string[] words = text.Split(separators, StringSplitOptions.RemoveEmptyEntries).Distinct().ToArray();
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            Parallel.ForEach(words, word =>
+            {
+                int count = words.Where(ex => ex.Equals(word)).Count();
+                lock (locker) { dict.Add(word, count); }
+            });
+            return dict;
+        }
+
+
+    }
+}
